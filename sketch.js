783442de3389921773a6c73e227f0c351ab42f16
kenaanhashem <71427375+kenaanhashem@users.noreply.github.com>
2020-10-02 09:57:39 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 let b;
 let anotherBall;
 let thirdBall;
@@ -6,6 +7,72 @@ let cloud2;
 
 
 
+=======
+let joe;
+let balls = [];
+
+function setup() {
+  createCanvas(800, 800);
+joe = new Sprite(width/1,200,4);
+
+}
+
+function draw() {
+  background(220);
+  joe.drawMe();
+  joe.moveMe();
+
+  if (frameCount % 25 == 0) {
+        let  b = new Ball(random(width), 0, -3);
+        balls.push(b);
+        console.log(balls);
+      }
+
+  	for (let i = 0; i < balls.length; i++) {
+  	 	      balls[i].drawBall();
+         	  balls[i].moveBall();
+          	balls[i].bounceBall();
+  	  }
+
+}
+
+class Ball {
+  constructor(x,y, speed){
+    this.x = x;
+
+this.y = y;
+this.speed = speed;
+  }
+
+  drawBall(){
+    stroke(0);
+    strokeWeight(1);
+    fill("red");
+      ellipse(this.x,this.y,10,10);
+  }
+  moveBall(){
+    this.x = this.x+ this.speed;
+    this.y = this.y+5;
+  }
+
+  bounceBall(){
+    if (this.x >= joe.x-15 && this.x <= joe.x+15 && this.y > joe.y-40 && this.y < joe.y+40){
+      this.speed = -this.speed;
+    }
+  }
+
+}
+
+
+
+
+
+
+
+
+
+class Sprite {
+>>>>>>> Stashed changes
 
 function setup() {
   createCanvas(800, 400);
@@ -29,6 +96,16 @@ function draw(){
       cloud2.drawCloud();
       cloud2.moveCloud();
 
+<<<<<<< Updated upstream
+=======
+drawMe(){
+  stroke(10);
+  fill("blue");
+  ellipse(this.x,this.y,75,75);
+  fill("black")
+  ellipse(this.x+7,this.y-5,5,5)
+  ellipse(this.x-7,this.y-5,5,5)
+>>>>>>> Stashed changes
 }
 
 
