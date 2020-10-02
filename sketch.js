6c4cb7c4841e30e288d305aef3055d0e249ece6f
noellewinn <71454923+noellewinn@users.noreply.github.com>
2020-10-02 09:58:24 -0700

    Update sketch.js
    i added one of the falling shapes classes, so i added the squares that will be some of the obstacles and will fall from above.

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let joe;
+let squares = [];
 
 function setup() {
   createCanvas(800, 800);
@@ -7,9 +8,21 @@ joe = new Sprite(width/1,200,4);
 
 function draw() {
   background(220);
+
   joe.drawMe();
   joe.moveMe();
 
+  if (frameCount % 25 == 0) {
+        let  b = new Squares(random(0,width), 0, random(-1,1), random(1,3));
+        squares.push(b);
+        console.log(squares);
+    }
+
+  for (let i = 0; i < squares.length; i++) {
+	 	      squares[i].drawSquares();
+       	  squares[i].moveSquares();
+        	squares[i].bounceSquares();
+    }
 }
 
 class Sprite {
@@ -50,5 +63,36 @@ moveMe(){
   }
 }
 
+} // end of class Sprite
 
+
+
+
+class Squares {
+
+  constructor(x,y, xspeed, yspeed){
+    this.x = x;
+    this.y = y;
+    this.xspeed = xspeed;
+    this.yspeed = yspeed
+  }
+
+  drawSquares(){
+    stroke(1);
+    strokeWeight(1);
+    fill("green");
+    rect(this.x,this.y,30,30);
+  }
+
+  moveSquares(){
+    this.x = this.x + this.xspeed;
+    this.y = this.y + this.yspeed;
+  }
+
+  bounceSquares(){
+    if (this.x >= joe.x-15 && this.x <= joe.x+15 && this.y > joe.y-40 && this.y < joe.y+40){
+        this.yspeed = -this.yspeed;
+        this.y=this.y-10;
+  }
+}
 }
